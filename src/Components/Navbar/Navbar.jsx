bfea10f59fba8931c2af7901d569376d98041e72
Gowreesh1905 <gowreeshoff@gmail.com>
2025-08-06 00:22:37 +0530

    navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 import './Navbar.css'
 
 import logo from '../../assets/logo.png'
+import search from '../../assets/search_icon.svg'
+import bell from '../../assets/bell_icon.svg'
+import profile from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Novels</li>
+          <li>Manga</li>
+          <li>Comics</li>
+          <li>Top Rated</li>
+        </ul>
+      </div>
+
+      <div className="navbar-right">
+        <img src={search} alt="" className='icons' />
+        <p>Children</p>
+        <img src={bell} alt="" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile} alt="" className='profile' />
+          <img src={caret_icon} alt="" />
+        </div>
+
       </div>
     </div>
   )
