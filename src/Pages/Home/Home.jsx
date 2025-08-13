a944eaf4c52313b2eb6f4364195bfcdf8ab6ffed
Gowreesh1905 <gowreeshoff@gmail.com>
2025-08-13 23:23:00 +0530

    home_page_v1

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
-import banner from '../../assets/main.jpg'
+import banner from '../../assets/aot-banner.jpg'
 import title from '../../assets/banner_title.png'
+import play from '../../assets/info_icon.png'
+import info from '../../assets/info_icon.png'
+
 const Home = () => {
   return (
     <div>
@@ -11,7 +14,12 @@ const Home = () => {
         <img src={banner} alt="" className='banner-img' />
         <div className="banner-title">
           <img src={title} alt="" />
+          <div className='btn-box'>
+            <btn className="btn"><img src={play} alt="" /> <p>Read</p></btn>
+            <btn className="btn dark-btn"><img src={info} alt="" /> <p>More Info</p></btn>
         </div>
+        </div>
+        
       </div>
     </div>
   )
